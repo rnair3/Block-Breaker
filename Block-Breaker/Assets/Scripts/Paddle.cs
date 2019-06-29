81d27aad7046e0e7a184da56517b3a201da3788e
Rinita Nair <rnair3@uncc.edu>
2019-06-29 11:54:01 -0400

    Paddle Movemwnt using mouse and attaching the ball to the paddle in the begin of the level

---
+++ Block-Breaker/Assets/Scripts/Paddle.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    //config parameters
+    [SerializeField] float minX = 1;
+    [SerializeField] float maxX = 15;
+
+    [SerializeField] int screenWidthInUnits;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(Input.mousePosition.x / Screen.width * screenWidthInUnits);
+        float x = Input.mousePosition.x / Screen.width * screenWidthInUnits;
+
+        Vector2 paddlePosition = new Vector2(transform.position.x, transform.position.y);
+        paddlePosition.x = Mathf.Clamp(x, minX, maxX);
+        transform.position = paddlePosition;
+
+    }
+}
