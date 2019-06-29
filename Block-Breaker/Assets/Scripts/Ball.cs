d689d1298053fd9020227cac64cfad354c825a5b
Rinita Nair <rnair3@uncc.edu>
2019-06-29 12:02:37 -0400

    Launching the ball on mouse click

--- Block-Breaker/Assets/Scripts/Ball.cs
+++ Block-Breaker/Assets/Scripts/Ball.cs
@@ -6,21 +6,42 @@ public class Ball : MonoBehaviour
 {
     //config parameters
     [SerializeField] Paddle paddle1;
+    [SerializeField] float xVelocity = 2f;
+    [SerializeField] float yVelocity = 15f;
 
     //states
     Vector2 paddleToBallVector;
+    private bool hasStarted;
 
     // Start is called before the first frame update
     void Start()
     {
         paddleToBallVector = transform.position - paddle1.transform.position;
+        hasStarted = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (!hasStarted)
+        {
+            LockToPaddle();
+            LaunchOnClick();
+        }
+    }
+
+    private void LockToPaddle()
+    {
         Vector2 paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
         transform.position = paddlePos + paddleToBallVector;
+    }
 
+    private void LaunchOnClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(xVelocity, yVelocity);
+            hasStarted = true;
+        }
     }
 }
